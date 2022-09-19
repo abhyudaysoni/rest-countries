98e68ca768894b07519651720c748400fdc0287b
abhyudaysoni <abhyudaysoni03@gmail.com>
2022-09-19 21:52:12 +0530

    now fetching data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import BodyContainer from "./components/BodyContainer/BodyContainer";
-// import useFetch from "./hooks/use-fetch";
-import data from "./data.json";
+import useFetch from "./hooks/use-fetch";
+// import data from "./data.json";
 import Loading from "./components/UI/loader/Loading";
 
 const url = `https://restcountries.com/v3.1/all`;
 
 function App() {
-  // const data = useFetch(url);
+  const data = useFetch(url);
   return (
     <div className="App">
       <Header />
