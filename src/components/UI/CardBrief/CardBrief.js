d628a416690d257af8b60dca87a285f9bcdcd300
abhyudaysoni <abhyudaysoni03@gmail.com>
2022-09-20 09:16:50 +0530

    removed population rounding off

--- src/components/UI/CardBrief/CardBrief.js
+++ src/components/UI/CardBrief/CardBrief.js
@@ -6,10 +6,7 @@ export default function CardBrief(props) {
   const classes = props.className;
   return (
     <NavLink to={`/details/${props.countryCode}`}>
-      <Container
-        className={classes}
-        id={props.id}
-      >
+      <Container className={classes} id={props.id}>
         <div className="flag-container-brief">
           <img src={props.flagSrc} alt="flag" />
         </div>
@@ -17,9 +14,7 @@ export default function CardBrief(props) {
           <h3>{props.countryName}</h3>
           <h5>
             Population:
-            {props.countryPopulation
-              ? (props.countryPopulation / 100000).toFixed(1) + "m"
-              : "Not Available"}
+            {props.countryPopulation}
           </h5>
           <h5>{`Region: ${props.coutryRegion}`}</h5>
           <h5>{`Capital: ${props.coutryCapital}`}</h5>
