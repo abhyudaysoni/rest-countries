7ef5a0d1292aa4431e9abd1cc1331a105cb454f3
abhyudaysoni <abhyudaysoni03@gmail.com>
2022-09-25 09:04:26 +0530

    removing everything from local storage

--- src/hooks/use-fetch.js
+++ src/hooks/use-fetch.js
@@ -3,7 +3,6 @@ import { useEffect, useState } from "react";
 const useFetch = (url) => {
   const [data, setData] = useState(null);
   useEffect(() => {
-    localStorage.setItem("isLightMode", true);
     const getData = () => {
       fetch(url)
         .then((response) => response.json())
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import { GlobalStyle } from "./globalStyles.js";
 
 const Root = () => {
-  const [isLight, setIsLight] = useState(true);
+  const [isLight, setIsLight] = useState(false);
   const themeHandler = () => {
     setIsLight((prev) => !prev);
   };
